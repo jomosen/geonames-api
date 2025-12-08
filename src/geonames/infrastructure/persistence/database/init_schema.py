4428a78954ffe552c586132b131c884f5a5134c5
jomosen <josemorellsendra@gmail.com>
2025-12-08 10:19:31 +0100

    Add init schema function

--- src/geonames/infrastructure/persistence/database/init_schema.py
+++ src/geonames/infrastructure/persistence/database/init_schema.py
@@ -6,6 +6,11 @@ from geonames.infrastructure.persistence.database.base import GeonamesBase
 
 def init_schema(engine: Engine):
 
-    import geonames.infrastructure.persistence.models
+    import geonames.infrastructure.persistence.models.geoname_base_model
+    import geonames.infrastructure.persistence.models.geoname_model
+    import geonames.infrastructure.persistence.models.alternate_name_model
+    import geonames.infrastructure.persistence.models.country_model
+    import geonames.infrastructure.persistence.models.admin_division_model
+    import geonames.infrastructure.persistence.models.city_model
 
     GeonamesBase.metadata.create_all(bind=engine)
